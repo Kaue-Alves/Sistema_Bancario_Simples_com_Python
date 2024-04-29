9257bf40129c76a675bcc77188b1ec8e7ac01b90
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-04-29 17:25:07 -0300

    Versão Final do Sistema Bancario Simples

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,57 +1,66 @@
-# Criar um sistema bancário com as operações: sacar, depositar e visualizar extrato
-
-# Será permitido realizar 3 saques diários com limite máximo de R$ 500. Todos os saques devem ser armazenados em uma variável e exibidos na operação extrato.
-
-# Extrato: deve listar todos os depósitos e saques realizados na conta. No fim da listagem deve ser exibido o saldo atual da conta. "R$ xxx.xx"
+LIMITE = 500
+LIMITE_SAQUES = 3
+saldo = 0
+extrato = "\n"
+numero_saques = 0
 
 menu = """
+ ===== MENU =====
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-
-"""
-
-saldo = 0
-LIMITE = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+  [d] Depositar
+  [s] Sacar
+  [e] Extrato
+  [q] Sair
+  => """
 
 while True:
 
     opcao = input(menu)
 
     if opcao == "d":
-        print("\nDepósito\n")
-        valor = float(input("Informe o valor do depósito: "))
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-            print(f"\nDepósito de R$ {valor:.2f} realizado com sucesso.\n")
-        else:
-            print("\nNão foi possível realizar o depósito. Tente novamente.\n")
+        print("\n ===== DEPÓSITO =====\n")
+        valor = input(" Informe o valor do depósito: ")
 
+        try:
+            valor = float(valor)
+            if valor > 0:
+                saldo += valor
+                extrato += f" Depósito: R$ {valor:.2f}\n"
+                print(f"\n Depósito de R$ {valor:.2f} realizado com sucesso.\n")
+            else:
+                print("\n Não foi possível realizar o depósito. Tente novamente.\n")
+        except:
+            print(" Valor digitado incorretamente.")
+
+            
     elif opcao == "s":
-        print("\nSaque\n")
-        valor = float(input("Informe o valor do saque: "))
-        if valor > 0 and valor <= saldo and valor <= LIMITE and numero_saques < LIMITE_SAQUES:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-            print(f"\nSaque de R$ {valor:.2f} realizado com sucesso.\n")
-        else:
-            print("\nNão foi possível realizar o saque. Possiveis motivos:\nSaldo insuficiente\nLimite de saques diários alcançado.\nValor do saque maior que o limite.\nValor digitado incorretamente.")
+        print("\n ===== SAQUE =====\n")
+        valor = input(" Informe o valor do saque: ")
+
+        try:
+            valor = float(valor)
+            if valor > 0 and valor <= saldo and valor <= LIMITE and numero_saques < LIMITE_SAQUES:
+                saldo -= valor
+                extrato += f" Saque: R$ {valor:.2f}\n"
+                numero_saques += 1
+                print(f"\n Saque de R$ {valor:.2f} realizado com sucesso.\n")
+            else:
+                print("\n Não foi possível realizar o saque. Possiveis motivos:\n Saldo insuficiente\n Limite de saques diários alcançado.\n Valor do saque maior que o limite.\n Valor digitado incorretamente.")
+        except:
+            print(" Valor digitado incorretamente.")
+
 
     elif opcao == "e":
-        print("\nExtrato\n")
-        print(f"{extrato}\n\nSaldo atual: R$ {saldo:.2f}.")
+        print("\n ===== EXTRATO =====")
+        if extrato:
+            print(f" {extrato}\n\n Saldo atual: R$ {saldo}.")
+        else:
+            print(f" Nenhuma operação foi realizada.\n\n Saldo atual: R$ {saldo}.")
 
         
     elif opcao == "q":
-        print("\nSaindo...")
+        print("\n Saindo...")
         break
 
     else:
-        print("\nOpção inválida, por favor selecione novamente a operação desejada.")
+        print("\n Opção inválida, por favor selecione novamente a operação desejada.")
